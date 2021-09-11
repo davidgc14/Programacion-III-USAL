8970d7627a58b6426ce92bf67230e206721c1dd6
Curbelo <curbelo.gc@gmail.com>
2021-09-11 18:29:05 +0200

    Nueva clase (booleana)

---
+++ VS_code/ejemplo/JavasimpleProyect/src/Booleano.java
@@ -0,0 +1,16 @@
+public class Booleano {
+    
+    int VarGlobal = 2; // Asi queda definida una variable globalmente
+    
+    public static void main(String[] args) {
+        boolean TF = false;
+        
+        if (TF == true){
+            System.out.println("El booleano vale verdadero");
+            System.out.println("Es necesario añadir corchete para varias lineas");
+        }
+
+        else
+            System.out.println("Es falso");
+    }
+}
