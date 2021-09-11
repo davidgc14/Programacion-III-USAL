7089744ddf44afb801f5a56f6dade201531d0b9c
Curbelo <curbelo.gc@gmail.com>
2021-09-11 15:07:33 +0200

    first commit

---
+++ VS_code/ejemplo/JavasimpleProyect/src/App.java
@@ -0,0 +1,11 @@
+class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("This is an app");
+    }
+}
+
+class Example {
+    public static void main(String[] args) {
+        System.out.println("This is another example");
+    }
+}
---
+++ VS_code/ejemplo/JavasimpleProyect/src/ClasePrincipal.java
@@ -0,0 +1,15 @@
+public class ClasePrincipal {
+    // definimos nueva variable numérica con el mismo nombre de la clase
+    int Numero = 20;
+    // esta variable se ha definido antes del main, es una variable "global"?
+
+    // construimos el main
+    public static void main(String[] args) {
+        
+        ClasePrincipal AlgoIntermedio = new ClasePrincipal();
+
+        System.out.println(AlgoIntermedio);
+
+        System.out.println(AlgoIntermedio.Numero);
+    }
+}
