238eeee6e1fc271263cb07ad07bbee723b130a63
Curbelo <curbelo.gc@gmail.com>
2021-09-11 19:07:34 +0200

    Ejercicios booleano

--- VS_code/ejemplo/JavasimpleProyect/src/Booleano.java
+++ VS_code/ejemplo/JavasimpleProyect/src/Booleano.java
@@ -6,11 +6,23 @@ public class Booleano {
         boolean TF = false;
         
         if (TF == true){
-            System.out.println("El booleano vale verdadero");
+            System.out.println("El booleano vale verdadero: " + TF);
             System.out.println("Es necesario añadir corchete para varias lineas");
         }
-
         else
-            System.out.println("Es falso");
+            System.out.println("Es falso: " + TF + ". Queda claro el uso del '+'");
+    
+        // Version compacta:
+        // Como vale false, la primera no se ejecuta, pero la segunda si
+
+        if(TF) System.out.println("Esto no se muestra porque vale false"); 
+        
+        TF = true;
+        if(TF) System.out.println("Esto ahora si se muestra"); 
+
+        // Implementación alternativa y directa
+
+        System.out.println(9 > 10); // Lo cual es una afirmación falsa, y así lo ahce ver por pantalla
+
     }
 }
