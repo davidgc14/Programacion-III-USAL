a15c8b25a34bc6e8b91069ebefcbe624fff86eb5
Curbelo <curbelo.gc@gmail.com>
2021-09-13 16:51:15 +0200

    Uso de datos no inicializados

--- VS_code/ejemplo/JavasimpleProyect/src/HelloWorld.java
+++ VS_code/ejemplo/JavasimpleProyect/src/HelloWorld.java
@@ -2,6 +2,8 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!"); //Comentario
 
+        // int x;
+        //System.out.println(x); 
+        // No se muestra la variable hasta que se inicializa, da error
     }
-
 }
