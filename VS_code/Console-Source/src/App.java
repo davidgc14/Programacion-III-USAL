04a6c199eb1ec325b68291592a040b79fd4e8b1e
Curbelo <curbelo.gc@gmail.com>
2021-09-23 10:51:41 +0200

    uso de .err y .out

--- VS_code/Console-Source/src/App.java
+++ VS_code/Console-Source/src/App.java
@@ -1,5 +1,12 @@
+// import static System.err.*;
+// import static System.out.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // supuestamente no sería necesario usar System
+        System.out.printf("Normal%n"); // Se pone esta orden en la cola de procesos
+        System.err.printf("Error%n"); // Interrumpe cualquier proceso y prioriza la impresion del mensaje
+
+        // Pero VSCode no tiene colores en terminal, solo NetBeans
     }
 }
