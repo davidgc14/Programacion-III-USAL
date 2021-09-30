cad99d2d19c73ba17177011a6bb5b55fcf676526
Curbelo <curbelo.gc@gmail.com>
2021-09-30 20:12:55 +0200

    Primer contacto con usos de bibliotecas externas

--- NetBeansProjects/Clases/Ejercicio05_biblioteca/src/ejercicio05_biblioteca/Ejercicio05_biblioteca.java
+++ NetBeansProjects/Clases/Ejercicio05_biblioteca/src/ejercicio05_biblioteca/Ejercicio05_biblioteca.java
@@ -1,20 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejercicio05_biblioteca;
 
-/**
- *
- * @author curbelo
- */
+import com.coti.tools.DiaUtil;
+import static com.coti.tools.Esdia.readDouble;
+import static com.coti.tools.Esdia.readFloat;
+
+// import java.util.Scanner;
+
 public class Ejercicio05_biblioteca {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        float pesoA, pesoB, pesoC;
+        double tallaA, tallaB, tallaC;
+        // String temp = "";
+        
+        // Scanner sc = new Scanner(System.in);
+
+        // leemos los pesos
     
-}
+        pesoA = readFloat("Cuanto pesa Tom ?  ");
+        pesoB = readFloat("Cuanto pesa Dick ?  ");
+        pesoC = readFloat("Cuanto pesa Harry ?  ");
+
+        // leemos las alturas
+    
+        tallaA = readDouble("Cuanto mide Tom ?  ");
+        tallaB = readDouble("Cuanto mide Dick ?  ");
+        tallaC = readDouble("Cuanto mide Harry ?  ");
+        
+        
+        // Calcular los máximos
+        final float pesoMax = Math.max(Math.max(pesoA, pesoB), pesoC);
+        final double tallaMax = Math.max(Math.max(tallaA, tallaB), tallaC);
+   
+
+        if (tallaA == tallaMax) {
+            System.out.println("El más alto es Tom : " + tallaA);
+        } else if (tallaB == tallaMax) {
+            System.out.println("El más alto es Dick : " + tallaB);
+        } else {
+            System.out.println("El más alto es Harry : " + tallaC);
+        } // end of if "alturas"
+
+
+        if (pesoA == pesoMax) {
+            System.out.println("El más pesado es Tom : " + pesoA);
+        } else if (pesoB == pesoMax) {
+            System.out.println("El más pesado es Dick : " + pesoB);
+        } else {
+            System.out.println("El más pesado es Harry : " + pesoC);
+        } // end of if "pesos"
+
+
+        DiaUtil.showFinalTime();
+    } // end of main
+    
+} // end of class
