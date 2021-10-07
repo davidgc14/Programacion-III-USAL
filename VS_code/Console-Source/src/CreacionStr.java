ba4a17c34c86ec39c6a72f1ea8b86f77d4c57adc
Curbelo <curbelo.gc@gmail.com>
2021-10-07 11:02:46 +0200

    Uso de memoria String

---
+++ VS_code/Console-Source/src/CreacionStr.java
@@ -0,0 +1,23 @@
+import static java.lang.System.out; // esto es para no tener que escribir System cuando queramos imprimir por pantalla
+
+public class CreacionStr {
+    
+    public static void main(String[] args) {
+
+        // creamos dos variables que hacen referencia al mismo espacio de memoria
+        String s = "Hola";
+        String q = s;
+        out.println("s vale " + s);
+        s = "mundo";
+        out.printf("%n s ahora  vale "
+                    + s
+                    + " y /Hola/ no se ha descartado %n");
+        out.printf("q sigue valiendo %s%n", q);
+
+        // otro ejemplo: creamos un nuevo espacio de memoria para la nueva variable
+        String t = "hola";
+        String u = new String(t); // creamos un nuevo hueco de memoria, un clon
+        out.printf("u vale igual que t: %s", u);
+
+    }
+}
--- VS_code/Console-Source/src/Wrapper.java
+++ VS_code/Console-Source/src/Wrapper.java
@@ -1,7 +1,9 @@
 public class Wrapper {
     public static void main(String[] args) {
         String s = "33";
-        Integer.parseInt(s);
+        int x = Integer.parseInt(s);
+
+        System.out.printf("Esta variable es un entero: " + x + "%n");
     }
     
 }
