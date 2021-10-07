6ab95de76fcaeced93bd47edf8d6cfab05102749
Curbelo <curbelo.gc@gmail.com>
2021-10-07 19:37:22 +0200

    Ejercicios De la clase práctica

---
+++ VS_code/Ejercicios de clase/src/sol_07.java
@@ -0,0 +1,32 @@
+import static com.coti.tools.Esdia.*; //ReadFloat y estas cosas
+import static com.coti.tools.DiaUtil.*;
+
+public class sol_07 {
+    public static void main(String[] args) {
+        final float IVA_FRUTA_FRESCA = 0.04f;
+
+        float kilosPeras, kilosManzs;
+        float preciokilosManzs, preciokilosPeras;
+        float totalSinIVA, ivaPagado, Totalpagado;
+        
+        clear();
+
+        kilosPeras = readFloat("Cuantos kilos de peras?");
+        kilosManzs = readFloat("Cuantos kilos de Manzs?");
+
+        preciokilosManzs = readFloat("Cuanto cuesta un kilo de manzs?");
+        preciokilosPeras = readFloat("Cuanto cuesta un kilo de Peras?");
+
+        totalSinIVA = kilosManzs*preciokilosManzs + 
+                        kilosPeras*preciokilosPeras;
+
+        ivaPagado = totalSinIVA * IVA_FRUTA_FRESCA;
+
+        Totalpagado = totalSinIVA + ivaPagado;
+
+        System.out.printf("Total sin IVA : %t%t%6.2f %n", totalSinIVA);
+        System.out.printf("IVA añadido   : %t%t%6.2f %n", ivaPagado);
+        System.out.printf("Total a pagar : %t%t%6.2f %n", Totalpagado);
+        
+    }
+}
