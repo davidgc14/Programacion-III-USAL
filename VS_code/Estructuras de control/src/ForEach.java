758cba2ef7ef7d0eda4c04c92acb9ec6067c9760
Curbelo <curbelo.gc@gmail.com>
2021-10-21 11:01:59 +0200

    Uso de funcion For each para bucles

---
+++ VS_code/Estructuras de control/src/ForEach.java
@@ -0,0 +1,10 @@
+public class ForEach {
+    public static void main(String[] args) throws Exception {
+        int[] lista = {1,2,3,4};
+        
+        for (int n : lista) {
+            System.out.println(n);
+
+        }// end for
+    }// end main
+}// end class
