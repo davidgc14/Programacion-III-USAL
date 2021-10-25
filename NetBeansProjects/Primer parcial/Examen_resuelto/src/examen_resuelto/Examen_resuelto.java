d62cef91cbb8fabc528b926ddccbece998becc15
Curbelo <curbelo.gc@gmail.com>
2021-10-25 09:34:02 +0200

    Primera prueba resuelta (Sin terminar)

---
+++ NetBeansProjects/Primer parcial/Examen_resuelto/src/examen_resuelto/Examen_resuelto.java
@@ -0,0 +1,33 @@
+package examen_resuelto;
+
+import com.coti.tools.*;
+
+
+public class Examen_resuelto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[] nom = new String[5];
+        int[] num = new int[5];
+        float[] pre = new float[5];
+        final float IVA = 0.33f;
+        System.out.println("ZAPATERIA");
+        
+        // leer nombre de los modelos
+        float costeSinIVA = 0.0f;
+        float masCaro = pre[0];
+        for (int i = 0 ; i < 5 ; i++) {
+            nom[i] = readSrting("Nombre del modelo           " + i + " : ");
+            num[i] = readInt   ("Numero de zapatos del modelo" + i + " : ");
+            pre[i] = readFloat ("Precio del modelo           " + nom[i] + " : ");
+            costeSinIVA += pre[i] * num[i];
+            if (pre[i] > masCaro){
+                masCaro = pre[i];
+            } // end if
+        
+        } // end for
+    } // end of main
+    
+} // end of class
