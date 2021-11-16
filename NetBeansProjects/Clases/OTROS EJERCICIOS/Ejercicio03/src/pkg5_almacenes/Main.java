784df7ea33d2ca6278ed9432eb9e3d89ce7c3419
Curbelo <curbelo.gc@gmail.com>
2021-11-16 13:02:50 +0100

    Actualización del enunciado

--- NetBeansProjects/Clases/OTROS EJERCICIOS/Ejercicio03/src/pkg5_almacenes/Main.java
+++ NetBeansProjects/Clases/OTROS EJERCICIOS/Ejercicio03/src/pkg5_almacenes/Main.java
@@ -1,5 +1,19 @@
 package pkg5_almacenes;
 
+/*
+    Una compañia tiene cinco almacenes, cada uno de los cuales  vente dos 
+    productos. Se dispone de una tabla para almacenar las ventas por unidades
+    de los dos productos en todos los almacenes. Se dispone tambien de los precios
+    que tiene cada unidad de los dos productos en todos los almacenes.
+
+*/
+
+/*
+Calcular el importe total de ventas de la empresa. Pedir la informacion anterior por
+teclado como resultado los datos en forma de tabla y el total de beneficios.
+*/
+
+
 import static com.coti.tools.DiaUtil.*;
 
 import view.View;
