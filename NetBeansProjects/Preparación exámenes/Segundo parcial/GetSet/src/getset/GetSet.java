489bec46bc3adea948d70124d7290590a3cdb16c
Curbelo <curbelo.gc@gmail.com>
2021-11-17 16:23:30 +0100

    Ejemplo sencillo del uso Get y Set

--- NetBeansProjects/Preparación exámenes/Segundo parcial/GetSet/src/getset/GetSet.java
+++ NetBeansProjects/Preparación exámenes/Segundo parcial/GetSet/src/getset/GetSet.java
@@ -3,7 +3,13 @@ package getset;
 public class GetSet {
 
     public static void main(String[] args) {
-        
+         
+        Persona p = new Persona();
+
+        p.setEdad(10);
+        p.setNombre("Miguelito");
+
+        System.out.printf("Tenemos al usuario %s con edad %d años%n", p.getNombre(), p.getEdad());
     }
     
 }
--- NetBeansProjects/Preparación exámenes/Segundo parcial/GetSet/src/getset/Persona.java
+++ NetBeansProjects/Preparación exámenes/Segundo parcial/GetSet/src/getset/Persona.java
@@ -3,4 +3,39 @@ package getset;
 
 public class Persona {
     
+    // datos de los ejemplares
+    private String nombre;
+    private int edad;
+
+    // CONSTRUCTOR
+    public Persona() {
+        this.nombre = "empty";
+        this.edad = -1; 
+    }
+
+    public Persona(String name, int old) {
+        this.nombre = name;
+        this.edad = old;         
+    }
+
+    // GETTERS: PARA poder pedir la informacion al ejemplar
+    // No modifica nada, solo proporciona informacion a otra clase
+    public int getEdad() {
+        return edad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    // SETTERS: para poder modificar datos del ejemplar
+    // no devuelve nada, solo modifica el valor pedido y se va
+    public void setEdad(int old) {
+        this.edad = old;
+    }
+    public void setNombre(String name) {
+        this.nombre = name;
+    }
+
+    
+
 }
