bbb4b91dcf25e0a08d81789812406959a2dcef5f
Curbelo <curbelo.gc@gmail.com>
2021-11-17 16:08:27 +0100

    Inicialización ejemplo Getters/Setters

---
+++ NetBeansProjects/Preparación exámenes/Segundo parcial/GetSet/src/getset/GetSet.java
@@ -0,0 +1,9 @@
+package getset;
+
+public class GetSet {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ NetBeansProjects/Preparación exámenes/Segundo parcial/GetSet/src/getset/Persona.java
@@ -0,0 +1,6 @@
+package getset;
+
+
+public class Persona {
+    
+}
