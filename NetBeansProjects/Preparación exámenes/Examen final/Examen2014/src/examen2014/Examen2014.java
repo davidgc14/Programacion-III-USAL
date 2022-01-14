f47590e53c3240ec29b8d65277459d260b1767e0
Curbelo <curbelo.gc@gmail.com>
2022-01-14 17:21:59 +0100

    Examen 2014 inicializado

---
+++ NetBeansProjects/Preparación exámenes/Examen final/Examen2014/src/examen2014/Examen2014.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen2014;
+
+/**
+ *
+ * @author curbelo
+ */
+public class Examen2014 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
